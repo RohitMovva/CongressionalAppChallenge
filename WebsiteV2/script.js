88f8155390d05f808c7a1df150a079fcaea077ca
Rohit Movva <tihormovva@gmail.com>
2023-10-30 21:10:53 -0600

    switched to only m/d/y

--- WebsiteV2/script.js
+++ WebsiteV2/script.js
@@ -225,7 +225,7 @@ function loadDashboard(){
                 var expand_button = document.createElement("button");
                 expand_button.classList.add("collapsible");
                 console.log(schedules[0][i][8]);
-                expand_button.innerText = "Created at: " + schedules[0][i][8];
+                expand_button.innerText = "Changed on " + schedules[0][i][8];
                 var expand_button_label = document.createElement("span");
                 expand_button_label.classList.add("plus")
                 expand_button_label.classList.add("accent")
